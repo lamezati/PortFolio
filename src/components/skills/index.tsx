2a6ec16ce476fe0da62f279b94fd632bc9fc77cd
Leonel Mezatio <afrokab1@gmail.com>
2025-03-01 21:22:19 -0500

    Update: Simplify skills index file exports

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -1,3 +1,3 @@
 // Export components from the skills directory
 export { default as SkillTag } from './SkillTag';
-export { default as TooltipProvider, useTooltip } from './TooltipContext';
+export { default as TooltipProvider } from './TooltipContext';
